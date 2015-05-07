5ab7cb2fd3ec2dbaf81f06f8f0950e237949a758
dseijasiglesias <dseijasiglesias@atanasoft12.danielcastelao.org>
2015-05-07 09:25:26 +0200

    add all varaibles whit their getters and setters

--- src/superficies/Modelo.java
+++ src/superficies/Modelo.java
@@ -7,6 +7,43 @@ package superficies;
 public class Modelo {
     
     private String shapeType;
+    private float area;
+    
+    private float length;
+    private float height;
+    private float radius;
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
     
     
     public String getShapeType() {
