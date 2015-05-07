8f749971728feb6dcbeebe3371d24b62ae8e0643
dseijasiglesias <dseijasiglesias@atanasoft12.danielcastelao.org>
2015-05-07 08:57:37 +0200

    inclued Object: Modelo, into askType(Modelo mod) void

--- src/superficies/Vista.java
+++ src/superficies/Vista.java
@@ -11,10 +11,16 @@ public class Vista {
     private static Scanner sc;
     
     
-    public static String askType(){
+    public static void askType(Modelo mod){
+        String shapeType;
+        
         System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
         sc = new Scanner(System.in);
-        return sc.nextLine();
+        shapeType = sc.nextLine();
+        
+        mod.setShapeType(shapeType);
+        
+        System.out.println("You said: " + shapeType);
     }
     
     
