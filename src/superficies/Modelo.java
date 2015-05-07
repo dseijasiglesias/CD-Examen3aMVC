5d4d967f29d4c9b70910a64427e817eb33f6e5cb
dseijasiglesias <dseijasiglesias@atanasoft12.danielcastelao.org>
2015-05-07 08:58:28 +0200

    add: private String shapeType whit get and set

---
+++ src/superficies/Modelo.java
@@ -0,0 +1,21 @@
+package superficies;
+
+/**
+ * @version 2.0
+ * @author Daniel Seijas
+ */
+public class Modelo {
+    
+    private String shapeType;
+    
+    
+    public String getShapeType() {
+        return shapeType;
+    }
+    
+    public void setShapeType(String shapeType) {
+        this.shapeType = shapeType;
+    }
+    
+    
+}
